05faf4081bfee405129bfb30e54687e822cbb0a2
Denis <denisp9831@gmail.com>
2025-10-01 15:20:49 +1000

    Add fonts

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,43 @@
 
 <template>
   <header>Header</header>
-  <main>Main</main>
+  <main>
+    <button>Кнопка</button>
+    <div class="div1">Надпись</div>
+    <div class="div2">Надпись</div>
+    <div class="div3">Надпись</div>
+    <div class="div4">Надпись</div>
+  </main>
   <footer>Footer</footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  display: flex;
+  min-width: 100%;
+  height: 100vh;
+  background: var(--color-background);
+  align-items: center;
+  justify-content: center;
+}
+button {
+  padding: 0 4rem 0 4rem;
+  height: 4rem;
+  background: var(--color-button-gradient);
+  color: var(--color-white);
+  border-radius: var(--radius-large);
+  font-size: 16px;
+}
+.div2 {
+  font-weight: var(--font-weight-medium);
+  color: var(--color-white);
+}
+.div3 {
+  font-weight: var(--font-weight-semibold);
+  color: var(--color-white);
+}
+.div4 {
+  font-weight: var(--font-weight-bold);
+  color: var(--color-white);
+}
+</style>
