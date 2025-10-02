33e32ed380b64bb8d9503f906f544f70f0af37c7
Denis <denisp9831@gmail.com>
2025-10-02 15:17:29 +1000

    Style fix

--- src/App.vue
+++ src/App.vue
@@ -13,33 +13,4 @@
   <footer>Footer</footer>
 </template>
 
-<style scoped>
-main {
-  display: flex;
-  min-width: 100%;
-  height: 100vh;
-  background: var(--color-background);
-  align-items: center;
-  justify-content: center;
-}
-button {
-  padding: 0 4rem 0 4rem;
-  height: 4rem;
-  background: var(--color-button-gradient);
-  color: var(--color-white);
-  border-radius: var(--radius-large);
-  font-size: 16px;
-}
-.div2 {
-  font-weight: var(--font-weight-medium);
-  color: var(--color-white);
-}
-.div3 {
-  font-weight: var(--font-weight-semibold);
-  color: var(--color-white);
-}
-.div4 {
-  font-weight: var(--font-weight-bold);
-  color: var(--color-white);
-}
-</style>
+<style scoped></style>
