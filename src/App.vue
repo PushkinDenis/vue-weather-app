b744d49f509065d52260a900598ec7dc410350ea
Denis <denisp9831@gmail.com>
2025-10-16 14:10:32 +1000

    Create button

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,7 @@
-<script setup></script>
+<script setup>
+import Button from "./components/Button/Button.vue";
+</script>
 
-<template>
-  <header>Header</header>
-  <main>
-    <button>Кнопка</button>
-    <div class="div1">Надпись</div>
-    <div class="div2">Надпись</div>
-    <div class="div3">Надпись</div>
-    <div class="div4">Надпись</div>
-    <img src="/icons/Sun.svg" alt="scsd" />
-  </main>
-  <footer>Footer</footer>
-</template>
+<template><Button /></template>
 
 <style scoped></style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <button class="button">Кнопка</button>
+</template>
+
+<style scoped>
+.button {
+  border-radius: var(--radius-sm);
+  background: var(--color-button-gradient);
+  padding: var(--spacing-sm);
+  font-size: var(--font-2xs);
+  font-weight: var(--font-weight-semibold);
+  color: var(--color-primary);
+  cursor: pointer;
+}
+.button:hover {
+  background: var(--color-button-gradient-inverted);
+}
+</style>
