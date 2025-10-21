cf6c3e7dba9e10f1080714e2ac0cea6a2824fe80
Denis <denisp9831@gmail.com>
2025-10-21 15:34:02 +1000

    Add input

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script setup>
+import { ref } from "vue";
 import Button from "./components/Button/Button.vue";
+import Input from "./components/Input/Input.vue";
+
+const username = ref("");
+const password = ref("");
 </script>
 
 <template>
-  <main class="main"><Button>Сохранить</Button></main>
+  <main class="main">
+    <Button>Сохранить</Button>
+    <Input v-model="username" placeholder="Введите город" name="input" />
+    <Input v-model="password" placeholder="Пароль" name="password" />
+    <p>Ваш город:{{ username }}</p>
+    <p>Ваш парол:{{ password }}</p>
+  </main>
 </template>
 
 <style scoped>
---
+++ src/components/Input/Input.vue
@@ -0,0 +1,37 @@
+<script setup>
+const { modelValue, placeholder, name } = defineProps({
+  modelValue: String,
+  placeholder: String,
+  name: String,
+});
+const emit = defineEmits(["update:modelValue"]);
+
+function handleInput(event) {
+  emit("update:modelValue", event.target.value);
+}
+</script>
+
+<template>
+  <input
+    :name="name"
+    :placeholder="placeholder"
+    :value="modelValue"
+    class="input"
+    @input="handleInput"
+  />
+</template>
+
+<style scoped>
+.input {
+  background: var(--color-bg-card);
+  color: var(--color-primary);
+  border-radius: var(--radius-sm);
+  padding: var(--spacing-sm) var(--spacing-md);
+  font-size: var(--font-2xs);
+  max-width: 100%;
+}
+
+.input::placeholder {
+  color: var(--color-primary-placeholder);
+}
+</style>
